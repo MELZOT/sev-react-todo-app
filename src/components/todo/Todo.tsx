f294292e3be541c0318b3b77d533d9390a4e3373
mzotika <mzotika@yahoo.gr>
2026-01-02 23:06:43 +0200

    feat(todos): persist todos to local storage

--- src/components/todo/Todo.tsx
+++ src/components/todo/Todo.tsx
@@ -2,9 +2,15 @@ import {useState, useRef, useEffect} from "react";
 import TodoForm from "./TodoForm.tsx";
 import TodoList from "./TodoList.tsx";
 import type { TodoProps } from "../../types";
+import Button from "../ui/Button.tsx";
+
+const getInitialTodos = (): TodoProps[] => {
+    const stored = localStorage.getItem("todos");
+    return stored ? JSON.parse(stored) : [];
+}
 
 const Todo = () => {
-    const [todos, setTodos] = useState<TodoProps[]>([]);
+    const [todos, setTodos] = useState<TodoProps[]>(getInitialTodos);
 
     const inputRef = useRef<HTMLInputElement>(null);
 
@@ -16,19 +22,62 @@ const Todo = () => {
         console.log(todos);
     }
 
+    const deleteTodo = (id: number) => {
+        setTodos(prev => prev.filter(todo => todo.id !== id));
+    }
+
+    const editTodo = (id: number, newText: string) => {
+        setTodos(prev => prev.map(todo => todo.id === id ? {...todo, text: newText} : todo))
+    }
+
+    const toggleTodo = (id: number) => {
+        setTodos(prev =>
+            prev.map(todo =>
+                todo.id === id ? { ...todo, completed: !todo.completed } : todo
+            )
+        )
+    }
+
+    const clearAllTodos = () => {
+        setTodos([]);
+    }
+
     useEffect(() => {
         inputRef.current?.focus();
+        localStorage.setItem("todos", JSON.stringify(todos))
     }, [todos]);
+
+    const totalTasks = todos.length;
+    const completedTasks = todos.filter(t => t.completed).length;
+    const activeTasks = totalTasks - completedTasks;
 
     return (
         <>
             <div className="max-w-sm mx-auto">
                 <h1 className="text-center text-2xl py-8">To-Do List</h1>
                 <TodoForm addTodo={addTodo} inputRef={inputRef} />
-                <TodoList todos={todos} />
+                <TodoList todos={todos} editTodo={editTodo} deleteTodo={deleteTodo} toggleTodo={toggleTodo} />
+                {totalTasks > 0 && (
+                    <>
+                        {/* TODO: add this to another component */}
+                        <div className="flex justify-between border-t pt-2 mt-4 text-cf-gray">
+                            <span>Total: {totalTasks}</span>
+                            <span>Active: {activeTasks}</span>
+                            <span>Completed: {completedTasks}</span>
+                        </div>
+                        <div className="text-end mt-4">
+                            <Button
+                                addClasses="bg-cf-dark-red"
+                                label="Clear All"
+                                onClick={clearAllTodos}
+                            />
+                        </div>
+                    </>
+                )}
             </div>
         </>
     )
 }
+
 
 export default Todo;
